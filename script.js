c3d17de6304e3540260f5fb3dea4aff167463bbe
Minku Singh <mailminkusingh@gmail.com>
2022-06-19 10:25:23 +0530

    feat: nav-res

---
+++ script.js
@@ -0,0 +1,18 @@
+let hamburger = document.querySelector(".hamburger");
+let cross = document.querySelector(".cross");
+let navList = document.querySelector("nav>ul.list");
+
+hamburger.addEventListener("click", toggleList);
+cross.addEventListener("click", toggleList);
+
+function toggleList(){
+    if(navList.hasAttribute("id","show-list")){
+        navList.removeAttribute("id", "show-list");
+        hamburger.removeAttribute("id", "hide-hamburger");
+        cross.removeAttribute("id", "show-cross");
+    }else{
+        navList.setAttribute("id", "show-list");
+        hamburger.setAttribute("id", "hide-hamburger");
+        cross.setAttribute("id", "show-cross");
+    }
+}
